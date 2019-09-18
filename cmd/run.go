6b434ba1e8076d055720eb90ae3cf885545b5fcd
Metalblueberry <metalblueberry@gmail.com>
2019-09-18 21:41:45 +0200

    add usage for run command

--- cmd/run.go
+++ cmd/run.go
@@ -32,15 +32,10 @@ import (
 )
 
 var runCmd = &cobra.Command{
-	Use:   "run",
+	Use:   "run <taskName>",
 	Short: "Run the desired task",
 	Args:  cobra.MaximumNArgs(1),
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Refer to external documentation at https://foosoft.net/projects/homemaker/`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		taskName := "default"
